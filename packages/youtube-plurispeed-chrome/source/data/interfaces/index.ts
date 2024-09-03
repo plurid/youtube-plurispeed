e3d7ccb6acc72007fbcdd82e386f09aa45910272
ly3xqhl8g9 <ly3xqhl8g9@plurid.com>
2024-09-03 13:02:09 +0300

    youtube-plurispeed-chrome > dev: data

--- packages/youtube-plurispeed-chrome/source/data/interfaces/index.ts
+++ packages/youtube-plurispeed-chrome/source/data/interfaces/index.ts
@@ -11,6 +11,10 @@ export type Speaker = {
 
 export type SpeakersData = {
     labels: string[];
-    segments: [number, number, number][];
+    // speaker ID = -2 => speech gap
+    // speaker ID = -1 => overlap
+    // speaker ID >= 0 => speaker ID
+    // speaker ID, start, stop, word count
+    segments: [number, number, number, number][];
 }
 // #endregion exports
